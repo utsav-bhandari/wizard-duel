dfd1b59b79cdf8fb61e377cd2a9d891e40e09531
nopeless <38830903+nopeless@users.noreply.github.com>
2024-04-15 17:46:22 -0500

    test: easier usage

--- src/main/java/io/github/utsav_bhandari/Scripts/StdDrawProvider.java
+++ src/main/java/io/github/utsav_bhandari/Scripts/StdDrawProvider.java
@@ -5,12 +5,29 @@ import io.github.utsav_bhandari.Lib.StdDrawBridge;
 import java.awt.*;
 import java.util.function.Consumer;
 
-public class StdDrawProvider {
-    public static void run(Consumer<Graphics2D> cb) {
-        StdDrawBridge.init(1920, 1080);
+public final class StdDrawProvider {
+    private static boolean initialized = false;
+    private static boolean running = false;
 
+    public static void init() {
+        if (!initialized) {
+            StdDrawBridge.init(1920, 1080);
+            initialized = true;
+        }
+    }
+
+    public static void run(Consumer<Graphics2D> cb) {
         StdDrawBridge.addCallback(cb);
 
+        if (running) {
+            return;
+        }
+
+        init();
+
+
         StdDrawBridge.run();
+
+        running = true;
     }
 }
