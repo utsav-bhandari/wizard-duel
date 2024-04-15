d51a048e1f7f7cfd644730d670c2faf623446dfe
nopeless <38830903+nopeless@users.noreply.github.com>
2024-04-15 18:40:09 -0500

    feat: waitKeyEvent api. This *might* be a mistake

--- src/main/java/io/github/utsav_bhandari/Engine/KeyboardInputHandler.java
+++ src/main/java/io/github/utsav_bhandari/Engine/KeyboardInputHandler.java
@@ -3,7 +3,6 @@ package io.github.utsav_bhandari.Engine;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class KeyboardInputHandler implements KeyListener {
@@ -11,9 +10,50 @@ public class KeyboardInputHandler implements KeyListener {
 
     protected String currentKeymap = null;
 
-    // Implement the keyPressed method
+    public static final int KEY_PRESSED = 1 << 0;
+    public static final int KEY_RELEASED = 1 << 1;
+    public static final int KEY_TYPED = 1 << 2;
+
+    public static final int KEY_ANY = KEY_PRESSED | KEY_RELEASED | KEY_TYPED;
+
+    protected final Object keyLock = new Object();
+    protected int keyEventPending = 0;
+
+    public void waitKeyEvent() {
+        waitKeyEvent(KEY_PRESSED);
+    }
+
+    // To future me
+    // I am not smart enough to understand whether it is better to do the keyEvent checking
+    // before the notify call or right here.
+    // I am no-where near capable of understanding the JVM internals on how this is implemented and what
+    // the overhead is.
+    // All I know is that this event will be triggered 3 times regardless of the key event type
+    // Just don't get confused later on
+    public void waitKeyEvent(int keyEventType) {
+        synchronized (keyLock) {
+            while ((keyEventPending & keyEventType) == 0) {
+                try {
+                    keyLock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        keyEventPending = 0;
+    }
+
+    protected void notifyLock(int keyEventType) {
+        keyEventPending |= keyEventType;
+        synchronized (keyLock) {
+            keyLock.notify();
+        }
+    }
+
     @Override
     public void keyPressed(KeyEvent e) {
+        notifyLock(KEY_PRESSED);
+
         if (currentKeymap == null) {
             return;
         }
@@ -31,10 +71,12 @@ public class KeyboardInputHandler implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        notifyLock(KEY_RELEASED);
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
+        notifyLock(KEY_TYPED);
     }
 
     public void addKeymap(String keymap, Iterable<Function<KeyEvent, Boolean>> handlers) {
