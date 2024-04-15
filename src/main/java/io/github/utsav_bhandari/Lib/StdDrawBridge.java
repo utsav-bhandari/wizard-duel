b81594a47a64f30c072fc2a13d6d2abe0be5327a
nopeless <38830903+nopeless@users.noreply.github.com>
2024-04-15 13:32:57 -0500

    deps: add StdDrawBridge and demo

---
+++ src/main/java/io/github/utsav_bhandari/Lib/StdDrawBridge.java
@@ -0,0 +1,130 @@
+package io.github.utsav_bhandari.Lib;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class StdDrawBridge {
+    public static JFrame frame;
+    ;
+    private static ArrayList<Consumer<Graphics2D>> callbacks = new ArrayList<>();
+    private static Graphics2D screen;
+
+    private static int frameCounter = 0;
+
+    private static final Timer frameCounterTimer = new Timer(1000, e -> {
+        System.out.println("FPS: " + frameCounter);
+        frameCounter = 0;
+    });
+
+    private static Timer timer;
+
+    public static void init(int width, int height) {
+        if (frame != null) {
+            throw new IllegalStateException(".init() can only be called once");
+        }
+
+        // Hack #1. Give correct dimensions to StdDraw, so it doesn't mess with the frame
+        // Unfortunately, there will be a initial window flicker and this cannot be disabled
+        StdDraw.setCanvasSize(width, height);
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(0, height);
+
+        // Hack #2. Enable double buffering in order to better optimize draw cycle
+        StdDraw.enableDoubleBuffering();
+
+        // Hack #3. Create custom offscreen buffer
+        var offscreenImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        screen = offscreenImage.createGraphics();
+        try {
+            // Hack #4. Expose frame for future use
+            frame = (JFrame) getField(StdDraw.class, "frame").get(null);
+
+            // Hack #5. Dispose screens because we don't need to use this
+            ((Graphics2D) getField(StdDraw.class, "onscreen").get(null)).dispose();
+            var offScreenField = (Field) getField(StdDraw.class, "offscreen");
+            ((Graphics2D) offScreenField.get(null)).dispose();
+
+            // Hack #6. Stub offscreen with our custom image buffer
+            offScreenField.set(null, screen);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Hack #7. Use non-retina Image for faster rendering (duh)
+        frame.setContentPane(new JLabel(new ImageIcon(offscreenImage)));
+
+        // Hack #8. Add rendering hints back
+        screen.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        screen.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        screen.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Set frame properties
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Remove menubar because it's ugly
+        frame.setJMenuBar(null);
+
+        // Trigger reflow. This is the final reflow and application will never change size
+        frame.pack();
+
+        JRootPane root = frame.getRootPane();
+        root.setDoubleBuffered(true);
+
+        Toolkit tk = root.getToolkit();
+
+
+        timer = new Timer(0, e -> {
+            screen.setBackground(Color.WHITE);
+            screen.clearRect(0, 0, frame.getWidth(), frame.getHeight());
+            for (Consumer<Graphics2D> cb : callbacks) {
+                cb.accept(screen);
+            }
+            frame.repaint();
+            frameCounter++;
+        });
+
+        timer.setCoalesce(true);
+    }
+
+    private static Field getField(Class<?> cls, String fieldName) {
+        for (Class<?> c = cls; c != null; c = c.getSuperclass()) {
+            try {
+                final Field field = c.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return field;
+            } catch (final NoSuchFieldException e) {
+                // Try parent
+            } catch (Exception e) {
+                throw new IllegalArgumentException(
+                        "Cannot access field " + cls.getName() + "." + fieldName, e);
+            }
+        }
+        throw new IllegalArgumentException(
+                "Cannot find field " + cls.getName() + "." + fieldName);
+    }
+
+    public static void addCallback(Consumer<Graphics2D> cb) {
+        callbacks.add(cb);
+    }
+
+    public static void run() {
+        frameCounterTimer.start();
+        timer.start();
+    }
+
+    public static void stop() {
+        frameCounterTimer.stop();
+        timer.stop();
+    }
+
+    public int getFramesPerSecond() {
+        return frameCounter;
+    }
+}
---
+++ src/main/java/io/github/utsav_bhandari/Scripts/StdDrawBridgeDemo.java
@@ -0,0 +1,45 @@
+package io.github.utsav_bhandari.Scripts;
+
+import edu.princeton.cs.algs4.StdDraw;
+import io.github.utsav_bhandari.Lib.StdDrawBridge;
+
+import java.awt.*;
+
+public class StdDrawBridgeDemo {
+    public static void main(String[] args) {
+        StdDrawBridge.init(1920, 1080);
+
+        final double[] angle = {0};
+
+        StdDrawBridge.addCallback((g) -> {
+            drawRotatingCylinder(angle[0], g);
+
+            angle[0] += 0.05;
+        });
+
+        StdDrawBridge.run();
+    }
+
+    private static void drawRotatingCylinder(double angle, Graphics2D g) {
+        int numCylinders = 500;
+        double angleIncrement = 2 * Math.PI / numCylinders;
+
+        g.setColor(Color.RED);
+        g.setStroke(new BasicStroke(5));
+
+//        g.drawRect(0, 0, 960, 540);
+        g.setColor(Color.getHSBColor((float) angle / 4.0f, 1f, 0.5f));
+//        StdDraw.rectangle(960, 540, 480, 270);
+
+        for (int i = 0; i < numCylinders; i++) {
+            double x = 960 + Math.cos(0.5 * angle + i * angleIncrement) * (270 + (i % 10 * 20));
+            double y = 540 + Math.sin(0.5 * angle + i * angleIncrement) * (270 + (i % 10 * 20));
+//            g.drawOval((int) x - 50, (int) y - 50, 100, 100);
+//            StdDraw.circle((int) x, (int) y, 50);
+            x = 960 + Math.cos(-angle + i * angleIncrement) * (250 + -(i % 10 * 20));
+            y = 540 + Math.sin(-angle + i * angleIncrement) * (250 + -(i % 10 * 20));
+//            g.drawOval((int) x - 50, (int) y - 50, 100, 100);
+            StdDraw.circle((int) x, (int) y, 50);
+        }
+    }
+}
