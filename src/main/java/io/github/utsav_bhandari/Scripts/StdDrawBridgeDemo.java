c3bcd0f95fdb9fcfd39e19443bfecf29861fc741
nopeless <38830903+nopeless@users.noreply.github.com>
2024-04-15 13:58:50 -0500

    feat: show coordinates on debug screen

--- src/main/java/io/github/utsav_bhandari/Scripts/StdDrawBridgeDemo.java
+++ src/main/java/io/github/utsav_bhandari/Scripts/StdDrawBridgeDemo.java
@@ -24,8 +24,27 @@ public class StdDrawBridgeDemo {
         int numCylinders = 500;
         double angleIncrement = 2 * Math.PI / numCylinders;
 
+
+        g.setColor(Color.PINK);
+        for (int i = 0; i < 1920; i += 100) {
+            for (int j = 0; j < 1080; j += 100) {
+                g.drawString("(" + i + ", " + j + ")", i, j);
+            }
+        }
+        g.fillRect(0, 0, 10, 10);
+
+        g.setColor(Color.CYAN);
+        for (int i = 0; i < 1920; i += 100) {
+            for (int j = 0; j < 1080; j += 100) {
+                StdDraw.text(i, j, "(" + i + ", " + j + ")");
+            }
+        }
+
+        StdDraw.filledRectangle(0, 0, 5, 5);
+
         g.setColor(Color.RED);
         g.setStroke(new BasicStroke(5));
+
 
 //        g.drawRect(0, 0, 960, 540);
         g.setColor(Color.getHSBColor((float) angle / 4.0f, 1f, 0.5f));
