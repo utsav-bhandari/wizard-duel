afce6c4dad7d76357d6cdd14d1d43700c4e2f232
nopeless <38830903+nopeless@users.noreply.github.com>
2024-04-16 16:33:06 -0500

    feat: resources

--- src/main/java/io/github/utsav_bhandari/Engine/Resource.java
+++ src/main/java/io/github/utsav_bhandari/Engine/Resource.java
@@ -2,7 +2,9 @@ package io.github.utsav_bhandari.Engine;
 
 import io.github.utsav_bhandari.Render.AnimatedSpriteRoot;
 
-import java.util.HashMap;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Singleton. Simple resource manager.
@@ -15,8 +17,35 @@ public final class Resource {
      */
     public final Class<AnimatedSpriteRoot> sprites = AnimatedSpriteRoot.class;
 
+    public static final BufferedImage NULL = loadResourceImage("/NULL.png");
+    public final BufferedImage titleScreen;
+
     private Resource() {
         // TODO: init stuff here
+        titleScreen = loadResourceImage("/sprites/title_screen.png");
+    }
+
+    /**
+     * Make sure to dispose if ephemeral used!!!
+     */
+    private static BufferedImage loadResourceImage(String path) {
+        BufferedImage image;
+
+        try (var stream = Resource.class.getResourceAsStream(path)) {
+            if (stream == null) {
+                System.out.println("Resource not found: " + path);
+                if (NULL == null) {
+                    throw new RuntimeException("NULL.png not found");
+                }
+
+                return NULL;
+            }
+            image = ImageIO.read(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return image;
     }
 
     public static Resource getInstance() {
